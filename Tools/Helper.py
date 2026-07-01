c2c550405347799c97de8f666e24683a5430bb25
ZenoSoup <anandawiko825@gmail.com>
2026-07-01 20:40:00 +0800

    Specified Helper prompt str something something

--- Tools/Helper.py
+++ Tools/Helper.py
@@ -1,19 +1,19 @@
 class Helper:
-    def inputInt(prompt):
+    def inputInt(prompt: str):
         while True:
             try:
                 return int(input(prompt))
             except ValueError:
                 print("Please only enter an integer value")
 
-    def inputFloat(prompt):
+    def inputFloat(prompt: str):
         while True:
             try:
                 return float(input(prompt))
             except ValueError:
                 print("Please only enter a float value")
 
-    def inputConfirmation(prompt):
+    def inputConfirmation(prompt: str):
         while True:
             confirmation = input(prompt).upper()
 
